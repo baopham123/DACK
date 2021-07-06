18935059991e6e640bdb20216b0040a43154c6bf
BẢO PHẠM CHÍ <18600026@student.hcmus.edu.vn>
2021-07-06 22:56:26 +0700

    source chinh

--- src/java/get/ProductGet.java
+++ src/java/get/ProductGet.java
@@ -21,29 +21,6 @@ import model.Product;
  */
 public class ProductGet {
 
-    //check
-public ArrayList<Product> getListProductByCategory(long category_id) throws SQLException {
-        Connection connection = DBConnect.getConnecttion();
-        String sql = "SELECT * FROM product";
-        PreparedStatement ps = connection.prepareCall(sql);
-        ResultSet rs = ps.executeQuery();
-        ArrayList<Product> list = new ArrayList<>();
-        while (rs.next()) {
-            
-            Product product = new Product();
-           
-            product.setProductID(rs.getLong("product_id"));
-            product.setProductName(rs.getString("product_name"));
-            product.setProductImage(rs.getString("product_image"));
-            product.setBrandID(rs.getLong("brand_id"));
-            product.setProductImageForward(rs.getString("product_image_forward"));
-            product.setProductImageBack(rs.getString("product_image_back"));
-            product.setProductPrice(rs.getLong("product_price"));
-            product.setProductDescription(rs.getString("product_description"));
-            list.add(product);
-        }
-        return list;
-    }
 //check
 public ArrayList<Product> getList3ProductByCategory(long category_id) throws SQLException {
         Connection connection = DBConnect.getConnecttion();
@@ -67,31 +44,6 @@ public ArrayList<Product> getList3ProductByCategory(long category_id) throws SQL
         }
         return list;
     }
-
-
-//Hiển thị chi tiết sản phẩm
-
-
-public Product getProduct(long productID) throws SQLException {
-     Connection connection = DBConnect.getConnecttion();
-     String sql = "SELECT * FROM product WHERE product_id = '" + productID + "'";
-     PreparedStatement ps = connection.prepareCall(sql);
-     ResultSet rs = ps.executeQuery();
-     Product product = new Product();
-     while (rs.next()) {
-          product.setProductID(rs.getLong("product_id"));
-          product.setCategoryID(rs.getLong("category_id"));
-          product.setProductName(rs.getString("product_name"));
-          product.setBrandID(rs.getLong("brand_id"));
-          product.setProductImage(rs.getString("product_image"));
-          product.setProductImageForward(rs.getString("product_image_forward"));
-          product.setProductImageBack(rs.getString("product_image_back"));
-          product.setProductPrice(rs.getLong("product_price"));
-          product.setProductDescription(rs.getString("product_description"));
-     }
-     return product;
-}
-
     public static void main(String[] args)throws SQLException {
       ProductGet get = new ProductGet();
         for(Product p : get.getListProductByCategory(2)){
